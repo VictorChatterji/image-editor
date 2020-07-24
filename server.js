3cb4065e008e90d71c8ddca0c13bb22ad63bed2e
Victor <chatterji.nani@gmail.com>
2020-07-24 23:48:25 +0530

    heroku deployment debugging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/image-editor'));
+app.use(express.static(__dirname + '/dist/image-editor'));  
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/image-editor/index.html'));
 });
