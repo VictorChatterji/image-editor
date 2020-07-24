8c25365425c243b5aa458fc9e2b6fec12e0a886f
Victor <chatterji.nani@gmail.com>
2020-07-24 23:36:11 +0530

    heroku deployment debugging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./dist/image-editor'));
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/image-editor/' });
 });
 app.listen(process.env.PORT || 8080);
