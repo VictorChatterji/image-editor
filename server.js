f7cb1c35243f9291871ce0afe30dfd7dd0990416
Victor <chatterji.nani@gmail.com>
2020-07-24 23:20:11 +0530

    deploying on heroku serverjs with latest packages

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.static('./dist/image-editor'));
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/image-editor/'}
-  );
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/image-editor/' });
 });
 app.listen(process.env.PORT || 8080);
